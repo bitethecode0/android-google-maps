60af815b6decdb77201826ff5d10fb422917204e
jooncsguy <joon.csguy@gmail.com>
2018-07-12 17:13:42 -0500

    7.12.2018, 5:10pm. Second, In MapActivity, getLoactionPermissions if it has all permissions, initMap(). Otherwise, it needs to get permissions first and initMap.

--- app/src/main/java/com/example/joon/googlemaps/MapActivity.java
+++ app/src/main/java/com/example/joon/googlemaps/MapActivity.java
@@ -1,13 +1,90 @@
 package com.example.joon.googlemaps;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.dynamic.SupportFragmentWrapper;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 public class MapActivity extends AppCompatActivity {
+
+    private static final String TAG = "MapActivity";
+    private static final int LOCATION_PERMISSION_REQUEST_CODE= 123;
+
+    //vars
+    private boolean mLocationPermissionGranted=false;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        getLocationPermissions();
     }
+
+
+
+    private void getLocationPermissions(){
+        Log.d(TAG, "getLocationPermissions: gettiong the location permissions.");
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(), permissions[0]) == PackageManager.PERMISSION_GRANTED  ){
+            if(ContextCompat.checkSelfPermission(this.getApplicationContext(), permissions[1])== PackageManager.PERMISSION_GRANTED){
+                mLocationPermissionGranted = true;
+                // if permissions were already granted, execute initMap()
+                initMap();
+            } else{
+                ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+            }
+
+        } else{
+            ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        mLocationPermissionGranted = false;
+
+        switch (requestCode){
+            case LOCATION_PERMISSION_REQUEST_CODE:{
+                if(grantResults.length>0){
+                    for(int i =0; i<grantResults.length; i++){
+                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                            mLocationPermissionGranted = false;
+                            Log.d(TAG, "onRequestPermissionsResult: permission failed");
+                            return;
+                        }
+                    }
+
+                    mLocationPermissionGranted = true;
+                    Log.d(TAG, "onRequestPermissionsResult: permission granted");
+                    initMap();
+                }
+            }
+        }
+    }
+
+    private void initMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                mMap = googleMap;
+            }
+        });
+    }
+
+
 }
